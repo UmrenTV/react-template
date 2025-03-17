38a21eac9fd22dca40cbd174f9beacb174a78e61
Jane Zdravevski <jane@zdravevski.pro>
2025-03-17 13:53:37 +0100

    basic react template init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            <h1 className="prose-2xl">Hello World!</h1>
+            <button className="btn btn-soft btn-secondary">Click Me</button>
+        </div>
+    );
+};
+export default App;
